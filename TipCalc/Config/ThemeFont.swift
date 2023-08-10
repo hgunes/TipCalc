9e1991936099d3b107cc839520956543dcb25f55
harungunes <h.gunes13@gmail.com>
2023-08-10 21:43:29 +0200

    feat: create label factory & configure logo view

--- TipCalc/Config/ThemeFont.swift
+++ TipCalc/Config/ThemeFont.swift
@@ -9,14 +9,18 @@ import UIKit
 
 struct ThemeFont {
     // AvenirNext
+    
+    // regular
     static func regular(ofSize size: CGFloat) -> UIFont {
         UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
     }
     
+    // bold
     static func bold(ofSize size: CGFloat) -> UIFont {
         UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
     }
     
+    // demibold
     static func demiBold(ofSize size: CGFloat) -> UIFont {
         UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
     }
