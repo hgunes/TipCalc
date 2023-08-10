6150234fd7ae0dc28dd5083eb8d1befb2955c41a
harungunes <h.gunes13@gmail.com>
2023-08-10 20:54:27 +0200

    feat: add layout constraints

---
+++ TipCalc/View/BillInputView.swift
@@ -0,0 +1,25 @@
+//
+//  BillInputView.swift
+//  TipCalc
+//
+//  Created by Harun Gunes on 10/08/2023.
+//
+
+import UIKit
+
+class BillInputView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .systemPurple
+    }
+}
---
+++ TipCalc/View/LogoView.swift
@@ -0,0 +1,25 @@
+//
+//  LogoView.swift
+//  TipCalc
+//
+//  Created by Harun Gunes on 10/08/2023.
+//
+
+import UIKit
+
+class LogoView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .red
+    }
+}
