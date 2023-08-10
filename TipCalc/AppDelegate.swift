c9280c86707ab19500bb1b8a1679b1f723a95c8d
harungunes <h.gunes13@gmail.com>
2023-08-10 20:12:02 +0200

    setup project

---
+++ TipCalc/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TipCalc
+//
+//  Created by Harun Gunes on 10/08/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+
+}
+
