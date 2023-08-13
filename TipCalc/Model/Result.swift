0e82e7a63f015efad5e6804a7c383f32c90a3322
harungunes <h.gunes13@gmail.com>
2023-08-13 11:32:02 +0200

    feat: add CalculatorVM

---
+++ TipCalc/Model/Result.swift
@@ -0,0 +1,8 @@
+//
+//  Result.swift
+//  TipCalc
+//
+//  Created by Harun Gunes on 13/08/2023.
+//
+
+import Foundation
---
+++ TipCalc/ViewModel/CalculatorVM.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorVM.swift
+//  TipCalc
+//
+//  Created by Harun Gunes on 13/08/2023.
+//
+
+import Foundation
