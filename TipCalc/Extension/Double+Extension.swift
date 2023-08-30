1254e8b80daf33e2d278a21bf96b25a3d51d3a8e
harungunes <h.gunes13@gmail.com>
2023-08-30 21:12:27 +0200

    feat: format currency values

---
+++ TipCalc/Extension/Double+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Double+Extension.swift
+//  TipCalc
+//
+//  Created by Harun Gunes on 30/08/2023.
+//
+
+import Foundation
+
+extension Double {
+    var currencyFormatted: String {
+        var isWholeNumber: Bool {
+            isZero ? true: !isNormal ? false: self == rounded()
+        }
+        let formatter = NumberFormatter()
+//        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = isWholeNumber ? 0 : 2
+        return formatter.string(for: self) ?? ""
+    }
+}
