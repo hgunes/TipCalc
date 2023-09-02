4c86ea1885825e817aac066efbb9aea4f39cd87f
harungunes <h.gunes13@gmail.com>
2023-09-02 09:50:25 +0200

    feat: add tap publishers

--- TipCalc/CalculatorVC.swift
+++ TipCalc/CalculatorVC.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SnapKit
 import Combine
+import CombineCocoa
 
 class CalculatorVC: UIViewController {
     
@@ -32,7 +33,25 @@ class CalculatorVC: UIViewController {
     }()
     
     private let vm = CalculatorVM()
+    
     private var cancellables = Set<AnyCancellable>()
+    
+    private lazy var viewTapPublisher: AnyPublisher<Void, Never> = {
+        let tapGesture = UITapGestureRecognizer(target: self, action: nil)
+        view.addGestureRecognizer(tapGesture)
+        return tapGesture.tapPublisher.flatMap { _ in
+            Just(())
+        }.eraseToAnyPublisher()
+    }()
+    
+    private lazy var logoViewTapPublisher: AnyPublisher<Void, Never> = {
+        let tapGesture = UITapGestureRecognizer(target: self, action: nil)
+        tapGesture.numberOfTapsRequired = 2
+        logoView.addGestureRecognizer(tapGesture)
+        return tapGesture.tapPublisher.flatMap { _ in
+            Just(())
+        }.eraseToAnyPublisher()
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +59,7 @@ class CalculatorVC: UIViewController {
         
         layout()
         bind()
+        observe()
     }
     
     private func bind() {
@@ -53,7 +73,16 @@ class CalculatorVC: UIViewController {
         output.updateViewPublisher.sink { [unowned self] result in
             resultView.configure(result: result)
         }.store(in: &cancellables)
+    }
+    
+    private func observe() {
+        viewTapPublisher.sink { [unowned self] value in
+            view.endEditing(true)
+        }.store(in: &cancellables)
         
+        logoViewTapPublisher.sink { _ in
+            print("logo view tapped")
+        }.store(in: &cancellables)
     }
     
     private func layout() {
