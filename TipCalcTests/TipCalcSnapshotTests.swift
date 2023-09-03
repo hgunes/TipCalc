d25c68dad19c579b463cde3c42e727f7a2b1948a
harungunes <h.gunes13@gmail.com>
2023-09-03 16:06:40 +0200

    test: add snapshot tests with values and selections

--- TipCalcTests/TipCalcSnapshotTests.swift
+++ TipCalcTests/TipCalcSnapshotTests.swift
@@ -37,15 +37,44 @@ class TipCalcSnapshotTests: XCTestCase {
         assertSnapshot(matching: view, as: .image(size: size))
     }
     
+    func testResultViewWithValues() {
+        // given
+        let size = CGSize(width: screenWidth, height: 224)
+        let result = Result(
+            amountPerPerson: 100.25,
+            totalBill: 45,
+            totalTip: 60)
+        // when
+        let view = ResultView()
+        view.configure(result: result)
+        // then
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
     // MARK: - BillInputView
     
-    func testBillInputView() {
+    func testInitialBillInputView() {
         // given
         let size = CGSize(width: screenWidth, height: 56)
         // when
         let view = BillInputView()
         // then
         assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+    
+    func testBillInputViewWithValues() {
+        // given
+        let size = CGSize(width: screenWidth, height: 56)
+        let result = Result(
+            amountPerPerson: 100.25,
+            totalBill: 45,
+            totalTip: 60)
+        // when
+        let view = BillInputView()
+        let textField = view.allSubViewsOf(type: UITextField.self).first
+        textField?.text = "500"
+        // then
+        assertSnapshot(matching: view, as: .image(size: size))
     }
     
     // MARK: - TipInputView
@@ -61,6 +90,18 @@ class TipCalcSnapshotTests: XCTestCase {
         assertSnapshot(matching: view, as: .image(size: size))
     }
     
+    func testTipInputViewWithSelection() {
+        // given
+        let size = CGSize(width: screenWidth, height: 56+56+15)
+        
+        // when
+        let view = TipInputView()
+        let button = view.allSubViewsOf(type: UIButton.self).first
+        button?.sendActions(for: .touchUpInside)
+        // then
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
     // MARK: - SplitInputView
     
     func testInitialSplitInputView() {
@@ -70,5 +111,35 @@ class TipCalcSnapshotTests: XCTestCase {
         let view = SplitInputView()
         // then
         assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+    
+    func testSplitInputViewWithSelection() {
+        // given
+        let size = CGSize(width: screenWidth, height: 56)
+        // when
+        let view = SplitInputView()
+        let button = view.allSubViewsOf(type: UIButton.self).last
+        button?.sendActions(for: .touchUpInside)
+        // then
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+}
+
+extension UIView {
+    /**
+     This is a function to get subViews of a particular type from view recursively.
+     It would look recursively in all subviews and return back the subviews of the type T
+     */
+    func allSubViewsOf<T : UIView>(type : T.Type) -> [T]{
+        var all = [T]()
+        func getSubview(view: UIView) {
+            if let aView = view as? T{
+                all.append(aView)
+            }
+            guard view.subviews.count>0 else { return }
+            view.subviews.forEach{ getSubview(view: $0) }
+        }
+        getSubview(view: self)
+        return all
     }
 }
